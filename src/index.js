6fe717adcc5e7275fadf94e6cd243bc4e27d73a6
Santiago Zapata <zapataospinasantiago8@gmail.com>
2023-09-15 19:30:55 -0500

    Version 1.4

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Habilitar CORS para permitir solicitudes desde http://127.0.0.1:5500
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500/assets/pages/Usuarios.html');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
