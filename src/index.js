af84797ba4561d147418bed83d1c1000c0f90e2b
Santiago Zapata <zapataospinasantiago8@gmail.com>
2023-09-19 22:37:09 -0500

    Ultimos cambios

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const rolRoutes = require('./routes/roles');
 
 const app = express();
 
-// Habilitar CORS para permitir solicitudes desde http://127.0.0.1:5500
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
