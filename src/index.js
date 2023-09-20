f347cb809b1e024f5855443fd1df381ec7de5ced
Santiago Zapata <zapataospinasantiago8@gmail.com>
2023-09-20 00:13:25 -0500

    Ultimos cambios

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ const rolRoutes = require('./routes/roles');
 const app = express();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500, https://modulosindividuales.onrender.com');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-  });
+});
 
 //Puerto
 const port = process.env.PORT || 3000;
