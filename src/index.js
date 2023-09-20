63692769740c161757f24b662657cb49a7b72207
Santiago Zapata <zapataospinasantiago8@gmail.com>
2023-09-20 00:20:37 -0500

    Ultimos cambios

--- src/index.js
+++ src/index.js
@@ -7,7 +7,14 @@ const rolRoutes = require('./routes/roles');
 const app = express();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500, https://modulosindividuales.onrender.com');
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://modulosindividuales.onrender.com');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
